bb44495ba8cad91b499f1900de301fdbd153bec8
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-08 13:27:44 +0300

    fixed

--- lesson1/task3/src/App.jsx
+++ lesson1/task3/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return <div class ='greeting'>
   <div class ='greeting__title'>Hello, world!</div>
-  <div class ='greeting__text'>I'am learning React</div>
+  <div class ='greeting__text'>I'm learning React</div>
  </div>;
 };
 
