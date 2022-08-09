053dde0de2537b99fb37cbcb1d2d519469fb475a
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-09 11:54:02 +0300

    task2

--- lesson3/task1_2/src/Search.jsx
+++ lesson3/task1_2/src/Search.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { Component } from 'react';
+// using func
+// const SearchField = props => {
+//   return (
+//     <div className="search">
+//       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//       <div className="search__field">
+//         <input type="text" className="search__input" />
+//         <button className="search__button">Search</button>
+//       </div>
+//     </div>
+//   );
+// };
 
-const SearchField = props => {
-  return (
-    <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className="search__field">
-        <input type="text" className="search__input" />
-        <button className="search__button">Search</button>
+// using class
+class SearchField extends Component {
+  render() {
+    return (
+      <div className="search">
+        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+        <div className="search__field">
+          <input type="text" className="search__input" />
+          <button className="search__button">Search</button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default SearchField;
