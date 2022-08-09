b996e7825a05249e0cbbd37f719ec8f3a41009f5
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-09 18:15:03 +0300

    try

--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -4,12 +4,9 @@ import './index.scss';
 import Comment from './Comment';
 
 const rootElem = document.querySelector('#root');
-const userInfo = {
+const author = {
   name: 'Tom',
   avatarUrl: 'https://github.com/github.png?size=460',
 };
 
-ReactDOM.render(
-  <Comment user={userInfo} text="Good job!" date={new Date('2022-01-01')} />,
-  rootElem,
-);
+ReactDOM.render(<Comment user={author} text="Good job!" date={new Date('2022-01-01')} />, rootElem);
