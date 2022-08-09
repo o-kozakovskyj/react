dd7bcec0d7c5b05b3afc6868bc21f8184f041e36
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-09 22:33:54 +0300

    solution

---
+++ lesson3/home2/src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import moment from 'moment';
+
+// eslint-disable-next-line arrow-body-style
+const Profile = props => {
+  console.log(props.userData.firstName);
+  return (
+    <div className="profile">
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+      <div className="profile__birth">{`Was born ${moment(props.userData.birthDate).format(
+        'DD MMM YY',
+      )} in ${props.userData.birthPlace}`}</div>
+    </div>
+  );
+};
+export default Profile;
