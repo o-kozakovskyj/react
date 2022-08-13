8d21e0db82b5a5a70199df40f928b48202ab5a27
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-13 16:28:58 +0300

    fixed

--- lesson6/task3/src/Page.jsx
+++ lesson6/task3/src/Page.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Message from './Message';
 
-const text1 = 'Hello, world';
+const text1 = 'Hello, world!';
 const text2 = 'Another exciting text.';
 
 class Page extends Component {
