63e6463df4272e3b31ab1808d421f5cee4dd9e78
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-14 13:46:21 +0300

    try

--- lesson6/task1/src/UserGreeting.jsx
+++ lesson6/task1/src/UserGreeting.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class UserGreeting extends Component {
   render() {
-    return <h1>Hello, user😎</h1>;
+    return <h1>Hello, user 😎</h1>;
   }
 }
 export default UserGreeting;
