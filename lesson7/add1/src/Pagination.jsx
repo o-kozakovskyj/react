1e234c1001098e5afbefca8b185611adb3d7421b
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-16 16:29:20 +0300

    try

--- lesson7/add1/src/Pagination.jsx
+++ lesson7/add1/src/Pagination.jsx
@@ -2,32 +2,26 @@ import React, { Component } from 'react';
 
 class Pagination extends Component {
   render() {
-    let disabledPrev;
-    let disabledNext;
-    let isPrevPageAvailable;
-    let isNextPageAvailable;
+    const isPrevPageAvailable = this.props.currentPage === 1 ? '' : '←';
+    const isNextPageAvailable = this.props.currentPage === this.props.totalItems ? '' : '→';
 
-    if (this.props.currentPage === 1) {
-      isPrevPageAvailable = '';
-      disabledPrev = 'disabled';
-    } else {
-      isPrevPageAvailable = '←';
-      disabledPrev = '';
-    }
-    if (this.props.currentPage === this.props.totalItems) {
-      isNextPageAvailable = '';
-      disabledNext = 'disabled';
-    } else {
-      isNextPageAvailable = '→';
-      disabledNext = '';
-    }
     return (
       <div className="pagination">
-        <button type="button" className="btn" disabled={disabledPrev} onClick={this.props.goPrev}>
+        <button
+          type="button"
+          className="btn"
+          disabled={!isPrevPageAvailable}
+          onClick={this.props.goPrev}
+        >
           {isPrevPageAvailable}
         </button>
         <span className="pagination__page">{this.props.currentPage}</span>
-        <button type="button" className="btn" disabled={disabledNext} onClick={this.props.goNext}>
+        <button
+          type="button"
+          className="btn"
+          disabled={!isNextPageAvailable}
+          onClick={this.props.goNext}
+        >
           {isNextPageAvailable}
         </button>
       </div>
