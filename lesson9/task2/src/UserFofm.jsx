f6a7243ba580d86dd9c6f866fb5454c430c2aaaa
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-18 17:38:11 +0300

    try

--- lesson9/task2/src/UserFofm.jsx
+++ lesson9/task2/src/UserFofm.jsx
@@ -16,14 +16,14 @@ class UserForm extends Component {
     });
   };
 
-  onSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-  };
+  // onSubmit = e => {
+  //   e.preventDefault();
+  //   this.props.onSubmit(this.state);
+  // };
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.onSubmit}>
+      <form className="login-form" onSubmit={() => this.props.onSubmit(this.state)}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
