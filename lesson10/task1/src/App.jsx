212f9eaa2f4ba4f796c2d37a05378996ceb69e7d
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-21 11:01:08 +0300

    task1

---
+++ lesson10/task1/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import UserProfile from './UserProfile';
+import UserMenu from './UserMenu';
+
+class App extends Component {
+  state = {
+    userData: null,
+  };
+
+  componentDidMount() {
+    this.fetchUserData(this.props.userName);
+  }
+
+  fetchUserData = name => {
+    fetch(`https://api.github.com/users/${name}`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          userData: data,
+        }),
+      );
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
+export default App;
