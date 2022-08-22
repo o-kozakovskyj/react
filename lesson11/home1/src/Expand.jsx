3fdb92c117147b38a250faceb6ab5c8252b5182a
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-22 19:16:05 +0300

    try

--- lesson11/home1/src/Expand.jsx
+++ lesson11/home1/src/Expand.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 
 const Expand = ({ children, display, openContent, title }) => {
   const content = display === true ? <div className="expand__content">{children}</div> : null;
-  const arrow = display === true ? 'fas fa-chevron-up' : 'fas fa-chevron-down';
+  const arrow = display === true ? 'up' : 'down';
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn" onClick={openContent}>
-          <i className={arrow}></i>
+          <i className={`fas fa-chevron-${arrow}`}></i>
         </button>
       </div>
       {content}
