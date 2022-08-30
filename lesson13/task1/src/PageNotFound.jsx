11390248914625a3c1756ef5b3c7c19f45ed871c
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-30 21:14:30 +0300

    try

--- lesson13/task1/src/PageNotFound.jsx
+++ lesson13/task1/src/PageNotFound.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class PageNotFound extends Component {
-  render() {
-    return (
-      <div className="page__content">
-        <h1>404 😢</h1>
-        <Link to="/">Go home</Link>
-      </div>
-    );
-  }
+function PageNotFound() {
+  return (
+    <div className="page__content">
+      <h1>404 😢</h1>
+      <Link to="/">Go home</Link>
+    </div>
+  );
 }
 
 export default PageNotFound;
