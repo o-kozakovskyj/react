85438720b0d76919c976757573e9e2c69c5eb14d
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-30 19:35:00 +0300

    try

--- lesson13/task1/src/Home.jsx
+++ lesson13/task1/src/Home.jsx
@@ -1,16 +1,12 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Home extends PureComponent {
-  render() {
-    return (
-      <div className="page__content">
-        <h1>Home</h1>
-        <p>We are here 🗺</p>
-        <Link to="/products">To products</Link>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="page__content">
+    <h1>Home</h1>
+    <p>We are here 🗺</p>
+    <Link to="/products">To products</Link>
+  </div>
+);
 
 export default Home;
