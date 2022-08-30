40c7244e3b7fbf5a7f125f81a875449151a28f40
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-08-30 19:21:02 +0300

    try

--- lesson13/task1/src/Contacts.jsx
+++ lesson13/task1/src/Contacts.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Contacts extends Component {
-  render() {
-    return (
-      <div className="page__content">
-        <h1>Contacts</h1>
-        <p>We are here 🗺</p>
-        <Link to="/">Go home</Link>
-      </div>
-    );
-  }
-}
+const Contacts = () => (
+  <div className="page__content">
+    <h1>Contacts</h1>
+    <p>We are here 🗺</p>
+    <Link to="/">Go home</Link>
+  </div>
+);
 
 export default Contacts;
