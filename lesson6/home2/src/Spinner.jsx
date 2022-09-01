f0859efa5201b2328c7281c11e741e133393fb06
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-09-01 15:03:47 +0300

    aa

--- lesson6/home2/src/Spinner.jsx
+++ lesson6/home2/src/Spinner.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Spinner = ({ size }) => {
-  const spinnerStyles = {
-    display: 'inline - block',
-    border: `${size}px solid #ccc`,
-    borderLeftColor: 'transparent',
-    animation: 'spin .5s infinite linear',
-    borderRadius: '50%',
-  };
-  return <span className="spinner" style={spinnerStyles}></span>;
-};
+const Spinner = () => (
+  // const spinnerStyles = {
+  //   display: 'inline - block',
+  //   border: `${size}px solid #ccc`,
+  //   borderLeftColor: 'transparent',
+  //   animation: 'spin .5s infinite linear',
+  //   borderRadius: '50%',
+  // };
+  <span className="spinner"></span>
+);
 export default Spinner;
