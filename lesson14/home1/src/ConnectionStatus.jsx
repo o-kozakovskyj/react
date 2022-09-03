5f27890996b9a84585b742d9416084f1a4539fa4
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-09-03 18:22:57 +0300

    start

---
+++ lesson14/home1/src/ConnectionStatus.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+const ConnectionStatus = () => {
+  const [status, setStatus] = useState(true);
+  useEffect(() => {
+    const handleStatus = e => {
+      setStatus(e.target.navigator.onLine);
+    };
+    window.addEventListener('online', handleStatus);
+    window.addEventListener('offline', handleStatus);
+    return () => {
+      window.removeEventListener('online', this.handleStatus);
+      window.removeEventListener('offline', this.handleStatus);
+    };
+  }, []);
+
+  const classStyle = status === true ? 'status' : 'status status_offline';
+  const statusText = status === true ? 'online' : 'offline';
+  return <div className={classStyle}>{statusText}</div>;
+};
+
+export default ConnectionStatus;
