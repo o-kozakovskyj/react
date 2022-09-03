f010659362fc7de9fee3eb2f0b349d4fbdf7ff00
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-09-03 14:27:40 +0300

    fix

--- lesson14/task2/src/User.jsx
+++ lesson14/task2/src/User.jsx
@@ -1,26 +1,12 @@
 /* eslint-disable camelcase */
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-class User extends Component {
-  state = {
-    userData: null,
-  };
-
-  componentDidMount() {
-    this.fetchUserData();
-  }
-
-  componentDidUpdate(prevProps) {
-    const prevUser = prevProps.match.params.userId;
-    const currentUser = this.props.match.params.userId;
-    if (prevUser !== currentUser) {
-      this.fetchUserData();
-    }
-  }
-
-  fetchUserData() {
-    const { match } = this.props;
-    fetch(`https://api.github.com/users/${match.params.userId}`)
+const User = ({ match }) => {
+  const [userData, setUserData] = useState(null);
+  const { userId } = useParams();
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
       .then(res => {
         if (res.ok) {
           return res.json();
@@ -28,27 +14,23 @@ class User extends Component {
         throw new Error();
       })
       .then(userData => {
-        this.setState({
-          userData,
-        });
+        setUserData(userData);
       });
-  }
+  }, [match.params.userId]);
 
-  render() {
-    const { userData } = this.state;
-    if (!userData) {
-      return null;
-    }
-    const { name, location, avatar_url } = userData;
-    return (
-      <div className="user">
-        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-        <div className="user__info">
-          <span className="user__name">{name}</span>
-          <span className="user__location">{location}</span>
-        </div>
-      </div>
-    );
+  if (!userData) {
+    return null;
   }
-}
+  const { name, location, avatar_url } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
 export default User;
