72fa5e65edf4714f4ef22dfa2ce68599e5c647b7
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-09-15 13:41:27 +0300

    refact

--- lesson7/add1/src/UsersList.jsx
+++ lesson7/add1/src/UsersList.jsx
@@ -3,12 +3,6 @@ import Pagination from './Pagination';
 import User from './User';
 
 class UsersList extends React.Component {
-  constructor({ users }) {
-    super({ users });
-    this.itemsPerPage = 3;
-    this.totalItems = users.length;
-  }
-
   state = {
     currentPage: 1,
   };
@@ -24,7 +18,7 @@ class UsersList extends React.Component {
 
   goNext = () => {
     const { currentPage } = this.state;
-    if (currentPage < this.totalItems) {
+    if (currentPage < this.props.users.length) {
       this.setState({
         currentPage: currentPage + 1,
       });
@@ -32,21 +26,23 @@ class UsersList extends React.Component {
   };
 
   render() {
+    const itemsPerPage = 3;
+    const { users } = this.props;
     const { currentPage } = this.state;
-    const startIndex = (currentPage - 1) * this.itemsPerPage;
+    const startIndex = (currentPage - 1) * itemsPerPage;
     return (
       <div>
         <Pagination
           currentPage={currentPage}
           goPrev={this.goPrev}
           goNext={this.goNext}
-          totalItems={this.totalItems}
-          itemsPerPage={this.itemsPerPage}
+          totalItems={users.length}
+          itemsPerPage={itemsPerPage}
         />
         <ul>
-          {this.props.users
-            .map(user => <User key={user.id} {...user} />)
-            .filter((_, index) => index >= startIndex && index < startIndex + this.itemsPerPage)}
+          {users.slice(startIndex, startIndex + itemsPerPage).map(user => (
+            <User key={user.id} {...user} />
+          ))}
         </ul>
       </div>
     );
